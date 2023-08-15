5092eaa447abf7e59b2bd3314c626341b482d408
Gogen2003 <122785869+Gogen2003@users.noreply.github.com>
2023-08-15 16:52:43 +0300

    s
    sqsq

---
+++ app_icons/src/App.js
@@ -0,0 +1,23 @@
+import Aside from './Aside';
+import Content from './Content';
+import  './Design/app.css';
+import './Design/aside.css';
+import './Design/media_requests.css';
+function App() {
+      
+  return (
+    
+    <>
+      <div className="container">
+        <div  className="box" >
+          <Aside />
+          <Content />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
